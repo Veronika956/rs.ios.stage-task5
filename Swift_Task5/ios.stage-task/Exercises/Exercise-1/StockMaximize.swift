3530d61af35699b8c49c65a90301e6fdea39a812
Veronika <Veronikasazonova210@gmail.com>
2021-06-14 19:25:53 +0300

    feat: ex 1 add

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,27 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        if (prices.isEmpty || prices == [0]) { return 0}
+        
+        let last = prices.last!
+        var result = 0
+        var dif = 0
+        for (index, price) in prices.enumerated(){
+            if (price >= last) {
+                dif = last - price
+                if dif >= 0 { result += dif} else { result += 0 }
+            }else {
+                if prices[index + 1] > last {
+                    dif = prices[index + 1] - price
+                    result += dif
+                } else {
+                    dif = last - price
+                    result += dif
+                }
+            }
+            
+        }
+        return result
     }
+   
 }
